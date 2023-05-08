90c03de43f0f37f1731eb1155059fc86ee13e97c
Fabrício <fabriciolak12@gmail.com>
2023-05-08 10:23:20 -0300

    feat(use-case/question): add http controller and use prisma repository

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(appRoutes)
 
 app.get('/', (request, reply) => {
   console.log(request.method, request.url)
---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,54 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+
+  async update(userId: string, data: Prisma.UserUpdateInput) {
+    const user = await prisma.user.update({
+      data,
+      where: {
+        id: userId,
+      },
+    })
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
+  async findByUsername(username: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    })
+
+    return user
+  }
+}
